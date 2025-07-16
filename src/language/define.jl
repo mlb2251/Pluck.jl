d2d9d56ee2ac20e2a24dd6499ecd35b3afb06a02
Matthew Bowers <mlbowers@mit.edu>
2025-07-16 19:15:01 -0400

    define

--- src/language/define.jl
+++ src/language/define.jl
@@ -18,8 +18,11 @@ function define(name, str)
     name = Symbol(name)
     if haskey(DEFINITIONS, name)
         # @warn "definition for $name already exists, overwriting"
+    else
+        # only do dummy here bc of race condition with @define being called from many threads
+        # you dont want it to briefly be set to unit and get used like that by another thread
+        DEFINITIONS[name] = Definition(name, DUMMY_EXPRESSION)
     end
-    DEFINITIONS[name] = Definition(name, DUMMY_EXPRESSION)
     try
         expr = parse_expr(str)
         DEFINITIONS[name] = Definition(name, expr)
