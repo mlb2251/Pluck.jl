72f449d313777213db3746fa3bd198d6a2614482
Maddy Bowers <mlbowers@mit.edu>
2025-07-28 11:51:38 -0400

    cleanup deriv tests

--- src/Pluck.jl
+++ src/Pluck.jl
@@ -31,6 +31,8 @@ include("likelihood/posterior_sampling.jl")
 include("language/toplevel.jl")
 include("util/bdd_viz.jl")
 include("util/tests.jl")
+include("util/deriv_tests.jl")
+
 export get_rsdd_time, clear_rsdd_time!, @rsdd_time
 
 load_pluck_file(joinpath(@__DIR__, "language", "stdlib.pluck"))
