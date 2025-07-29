6d8815d67ec2d79a34fcc67585edcd24d490b929
Maddy Bowers <mlbowers@mit.edu>
2025-07-29 15:35:18 -0500

    tweak

--- src/likelihood/lazy_knowledge_compilation/lazy_knowledge_compilation.jl
+++ src/likelihood/lazy_knowledge_compilation/lazy_knowledge_compilation.jl
@@ -112,8 +112,6 @@ function compile(expr::PExpr, cfg::LazyKCConfig)
         return CompileResult(weighted_results, state.stats, worlds, state)
     end
 
-    @show state.stats.num_recursive_calls
-
     return weighted_results
 end
 
