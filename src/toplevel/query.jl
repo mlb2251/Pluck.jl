b487c9d3b16023d7217e93663604472027b70165
Maddy Bowers <mlbowers@mit.edu>
2025-12-13 14:52:56 -0500

    factor out some common bits in query

--- src/toplevel/query.jl
+++ src/toplevel/query.jl
@@ -36,38 +36,47 @@ function eval_query(val, state::LazyKCState)
 end
 
 function marginal_query(val, state, mode::ExactInference)
+    # (Marginal e) => simply force and enumerate the thunk `e`
     ret, _ = evaluate(val.args[1], state.manager.BDD_TRUE, state)
-    full_ret = infer_full_distribution(ret, state)
-    results = [v => RSDD.bdd_wmc(b) for (v, b) in full_ret]
-    return results
+    return full_dist_and_wmc(ret, state)
 end
 
 function marginal_query(val, state, mode::SMCInference)
+    # (Marginal e) in subproblem mode => force the thunk `e`, call subproblem_monte_carlo on result, then force and enumerate the result of that
     ret, _ = evaluate(val.args[1], state.manager.BDD_TRUE, state)
     ret_thunk_union, normalizer = subproblem_monte_carlo(ret, mode.k, state)
     ret, _ = evaluate(ret_thunk_union, state.manager.BDD_TRUE, state)
-    full_ret = infer_full_distribution(ret, state)
-    results = [v => RSDD.bdd_wmc(b) for (v, b) in full_ret]
-    return results
+    return full_dist_and_wmc(ret, state)
 end
 
 function posterior_query(val, state, mode::ExactInference)
-    env = EnvCons(:a, val.args[1], EnvCons(:b, val.args[2], EnvNil()))
-    given_expr = parse_expr("(given b a)"; env=["a", "b"])
-    # TODO: reconsider strict order index to use?
-    ret, _ = traced_compile_inner(given_expr, env, state.manager.BDD_TRUE, state, 0)
-    full_ret = infer_full_distribution(ret, state)
-    results = normalize([v => RSDD.bdd_wmc(b) for (v, b) in full_ret])
-    return results
+    ret = unnormalized_posterior(val, state, "given")
+    return normalize(full_dist_and_wmc(ret, state))
 end
 
 function posterior_query(val, state, mode::SMCInference)
-    env = EnvCons(:a, val.args[1], EnvCons(:b, val.args[2], EnvNil()))
-    given_expr = parse_expr("(given-suspend b a)"; env=["a", "b"])
-    ret, _ = traced_compile_inner(given_expr, env, state.manager.BDD_TRUE, state, 0)
+    ret = unnormalized_posterior(val, state, "given-suspend")
     ret_thunk_union, normalizer = subproblem_monte_carlo(ret, mode.k, state)
     ret, _ = evaluate(ret_thunk_union, state.manager.BDD_TRUE, state)
-    full_ret = infer_full_distribution(ret, state)
-    results = normalize([v => RSDD.bdd_wmc(b) for (v, b) in full_ret])
-    return results
+    return normalize(full_dist_and_wmc(ret, state))
 end
+
+"""
+convert WHNF results to fully resolved values, and run WMC
+"""
+function full_dist_and_wmc(ret, state)
+    full_ret = infer_full_distribution(ret, state)
+    return [v => RSDD.bdd_wmc(b) for (v, b) in full_ret]
+end
+
+"""
+(Posterior evidence expr) => compile the expression `(given evidence expr)` and return WHNF results
+"""
+function unnormalized_posterior(val, state, given) # given = "given" or "given-suspend"
+    env = EnvCons(:a, val.args[1], EnvCons(:b, val.args[2], EnvNil()))
+    given_expr = parse_expr("($given b a)"; env=["a", "b"])
+    # TODO: reconsider strict order index to use?
+    ret, _ = traced_compile_inner(given_expr, env, state.manager.BDD_TRUE, state, 0)
+    return ret
+end
+
