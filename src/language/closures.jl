fa8f31e31805958e144a905c1aa73f21b5f9ca11
Maddy Bowers <mlbowers@mit.edu>
2026-03-09 19:29:35 -0400

    cut closure equality and hash

--- src/language/closures.jl
+++ src/language/closures.jl
@@ -7,27 +7,6 @@ mutable struct Closure <: AbstractValue
     origin::PExpr # debug / stacktrace info
 end
 
-function Base.:(==)(x::Closure, y::Closure)
-    x.name == y.name || return false
-    x.expr == y.expr || return false
-    is_self_loop(x) && is_self_loop(y) && return tailenv(x.env) == tailenv(y.env)
-    x.env == y.env
-end
-function Base.hash(x::Closure, h::UInt)
-    h = hash(x.expr, h)
-    h = hash(x.name, h)
-    h = hash(length(x.env), h) # bleh having trouble stopping the self loops
-    return h
-end
 function Base.show(io::IO, c::Closure)
     print(io, "(Closure from $(c.origin) name=$(c.name) body=$(c.expr))")
 end
-
-function make_self_loop(body, env, recname, nonrecname, origin)
-    new_env = EnvCons(recname, missing, env)
-    closure = Closure(body, new_env, nonrecname, origin)
-    new_env.val = closure # overwrite the Missing with the closure itself
-    closure
-end
-
-is_self_loop(x::Closure) = !isempty(x.env) && fst(x.env) === x
