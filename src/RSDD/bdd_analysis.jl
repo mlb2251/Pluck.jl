377c5875ecfd2704ac8df048d7e26754da57490c
Maddy Bowers <mlbowers@mit.edu>
2026-03-23 08:57:39 -0400

    maybe_const_*

--- src/RSDD/bdd_analysis.jl
+++ src/RSDD/bdd_analysis.jl
@@ -12,13 +12,13 @@ end
 mutable struct DeferredBDD
     head::DeferredHead
     possible_vars::Set{Label}
-    maybe_true::Bool
-    maybe_false::Bool
+    maybe_const_true::Bool
+    maybe_const_false::Bool
     args::Vector{DeferredBDD}
 end
 
 function bdd_is_false(a::DeferredBDD)
-    if !a.maybe_false
+    if !a.maybe_const_false
         return false # this is the key – all our savings come from this
     end
     # fall back on forcing the BDD
@@ -53,8 +53,8 @@ end
 function set_forced(dbdd::DeferredBDD, bdd::BDD)
     dbdd.head = Forced(bdd)
     dbdd.possible_vars = bdd_get_vars(bdd)
-    dbdd.maybe_true = bdd_is_true(bdd)
-    dbdd.maybe_false = bdd_is_false(bdd)
+    dbdd.maybe_const_true = bdd_is_true(bdd)
+    dbdd.maybe_const_false = bdd_is_false(bdd)
     empty!(dbdd.args)
 end
 
@@ -65,23 +65,23 @@ end
 
 function bdd_and(a::DeferredBDD, b::DeferredBDD)
     possible_vars = a.possible_vars ∪ b.possible_vars
-    maybe_true = a.maybe_true && b.maybe_true
-    maybe_false = a.maybe_false || b.maybe_false || !isempty(a.possible_vars ∩ b.possible_vars)
-    return DeferredBDD(DeferredAnd(), possible_vars, maybe_true, maybe_false, [a, b])
+    maybe_const_true = a.maybe_const_true && b.maybe_const_true
+    maybe_const_false = a.maybe_const_false || b.maybe_const_false || !isempty(a.possible_vars ∩ b.possible_vars)
+    return DeferredBDD(DeferredAnd(), possible_vars, maybe_const_true, maybe_const_false, [a, b])
 end
 
 # note you could also write Or just as !(!a & !b) which if you had complement pointers wouldnt be bad
 # but none of this matters for proof of concept
 function bdd_or(a::DeferredBDD, b::DeferredBDD)
     possible_vars = a.possible_vars ∪ b.possible_vars
-    maybe_true = a.maybe_true || b.maybe_true || !isempty(a.possible_vars ∩ b.possible_vars)
-    maybe_false = a.maybe_false && b.maybe_false
-    return DeferredBDD(DeferredOr(), possible_vars, maybe_true, maybe_false, [a, b])
+    maybe_const_true = a.maybe_const_true || b.maybe_const_true || !isempty(a.possible_vars ∩ b.possible_vars)
+    maybe_const_false = a.maybe_const_false && b.maybe_const_false
+    return DeferredBDD(DeferredOr(), possible_vars, maybe_const_true, maybe_const_false, [a, b])
 end
 
 function bdd_negate(a::DeferredBDD)
     # maybe true and maybe false swap
-    return DeferredBDD(DeferredNot(), a.possible_vars, a.maybe_false, a.maybe_true, [a])
+    return DeferredBDD(DeferredNot(), a.possible_vars, a.maybe_const_false, a.maybe_const_true, [a])
 end
 
 bdd_implies(a::DeferredBDD, b::DeferredBDD) = b | !a
