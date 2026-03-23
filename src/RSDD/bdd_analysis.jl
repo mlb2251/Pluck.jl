67fd5610bb592107d7254589e726450ef89c5071
Maddy Bowers <mlbowers@mit.edu>
2026-03-23 13:21:08 -0400

    wip

--- src/RSDD/bdd_analysis.jl
+++ src/RSDD/bdd_analysis.jl
@@ -11,19 +11,60 @@ end
 
 mutable struct BDD
     head::DeferredHead
+    op::Symbol
     possible_vars::Set{Label}
+    possible_overlap::Set{Label}
     maybe_const_true::Bool
     maybe_const_false::Bool
     args::Vector{BDD}
 end
 
-function bdd_is_false(a::BDD)
-    if !a.maybe_const_false
-        return false # this is the key – all our savings come from this
+function bdd_is_false(bdd::BDD)
+    # if it could be false, we need to force it
+    if bdd_maybe_const_false(bdd)
+        return bdd_is_false(force(bdd))
     end
-    # fall back on forcing the BDD
-    return bdd_is_false(force(a))
+    return false
 end
+
+function bdd_maybe_const_false(bdd::BDD)
+    head = bdd.head
+
+    # if forced, we just look at the inner bdd
+    head isa Forced && return bdd_is_false(head.bdd)
+
+    if head isa DeferredNot
+        return bdd_maybe_const_true(bdd.args[1])
+    end
+
+    if head isa DeferredAnd
+        a = bdd.args[1]
+        b = bdd.args[2]
+        # the bdd is false if either child is false
+        bdd_maybe_const_false(a) && return true
+        bdd_maybe_const_false(b) && return true
+        # if neither is false and they have no overlap, then the bdd cant be false
+        isempty(bdd.possible_overlap) && return false
+        # if there is overlap, we need to force ourselves
+        return bdd_is_false(force(bdd))
+    end
+
+    if head isa DeferredOr
+        a = bdd.args[1]
+        b = bdd.args[2]
+        # the bdd is false if either child is false
+        bdd_is_false(a) && return true
+        bdd_is_false(b) && return true
+        # if neither is false and they have no overlap, then the bdd cant be false
+        isempty(bdd.possible_overlap) && return false
+        # if there is overlap, we need to force ourselves
+        return bdd_is_false(force(bdd))
+    end
+end
+
+
+
+
 
 function bdd_is_true(a::BDD)
     if !a.maybe_const_true
@@ -66,14 +107,15 @@ function set_forced(dbdd::BDD, bdd::InnerBDD)
 end
 
 function embed_bdd(bdd::InnerBDD)::BDD
-    return BDD(Forced(bdd), bdd_get_vars(bdd), bdd_is_true(bdd), bdd_is_false(bdd), BDD[])
+    return BDD(Forced(bdd), bdd_get_vars(bdd), Set{Label}(), bdd_is_true(bdd), bdd_is_false(bdd), BDD[])
 end
 
 
 function bdd_and(a::BDD, b::BDD)
     possible_vars = a.possible_vars ∪ b.possible_vars
+    possible_overlap = a.possible_overlap ∩ b.possible_overlap
     maybe_const_true = a.maybe_const_true && b.maybe_const_true
-    maybe_const_false = a.maybe_const_false || b.maybe_const_false || !isempty(a.possible_vars ∩ b.possible_vars)
+    maybe_const_false = a.maybe_const_false || b.maybe_const_false || !isempty(possible_overlap)
     return BDD(DeferredAnd(), possible_vars, maybe_const_true, maybe_const_false, [a, b])
 end
 
