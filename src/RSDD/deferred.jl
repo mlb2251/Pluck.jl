8a85d88a9f0acd4e0842811dc2e542aae14e1fb2
Maddy Bowers <mlbowers@mit.edu>
2026-03-24 15:58:11 -0400

    store min and max

--- src/RSDD/deferred.jl
+++ src/RSDD/deferred.jl
@@ -11,6 +11,8 @@ node(bdd::BDD) :: DeferredNode = bdd.node
 mutable struct DeferredNode
     op::Symbol
     possible_vars::Vector{Int}
+    min_var::Int
+    max_var::Int
     maybe_const_true::Bool
     maybe_const_false::Bool
     left::Union{BDD, Nothing}
@@ -73,17 +75,17 @@ end
 # end
 
 function var_bdd(bdd::InnerBDD, label::Int)::BDD
-    node = DeferredNode(:var, varset_of_label(label), false, false, nothing, nothing, bdd)
+    node = DeferredNode(:var, varset_of_label(label), label, label, false, false, nothing, nothing, bdd)
     return BDD(node, false)
 end
 
 function true_bdd(bdd::InnerBDD)::BDD
-    node = DeferredNode(:T, empty_varset(), true, false, nothing, nothing, bdd)
+    node = DeferredNode(:T, empty_varset(), -1, -1, true, false, nothing, nothing, bdd)
     return BDD(node, false)
 end
 
 function false_bdd(bdd::InnerBDD)::BDD
-    node = DeferredNode(:F, empty_varset(), false, true, nothing, nothing, bdd)
+    node = DeferredNode(:F, empty_varset(), -1, -1, false, true, nothing, nothing, bdd)
     return BDD(node, false)
 end
 
@@ -91,11 +93,13 @@ end
 function bdd_and(a::BDD, b::BDD)
     a_node = a.node :: DeferredNode
     b_node = b.node :: DeferredNode
+    min_var = min(a_node.min_var, b_node.min_var)
+    max_var = max(a_node.max_var, b_node.max_var)
     possible_vars = varset_union(a_node.possible_vars, b_node.possible_vars)
     empty_intersect = varset_empty_intersection(a_node.possible_vars, b_node.possible_vars)
     maybe_const_true_val = maybe_const_true(a) && maybe_const_true(b) || !empty_intersect
     maybe_const_false_val = maybe_const_false(a) || maybe_const_false(b) || !empty_intersect
-    node = DeferredNode(:and, possible_vars, maybe_const_true_val, maybe_const_false_val, a, b, nothing)
+    node = DeferredNode(:and, possible_vars, min_var, max_var, maybe_const_true_val, maybe_const_false_val, a, b, nothing)
     return BDD(node, false)
 end
 
