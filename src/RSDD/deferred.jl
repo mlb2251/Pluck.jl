2cfcdebecdcce632a7143afe4b75786fe0bd53a1
Maddy Bowers <mlbowers@mit.edu>
2026-03-24 10:55:51 -0400

    negate as ptr

--- src/RSDD/deferred.jl
+++ src/RSDD/deferred.jl
@@ -10,8 +10,12 @@ struct Forced <: DeferredHead
 end
 
 mutable struct BDD
-    head::DeferredHead
-    # op::Symbol
+    node
+    negated::Bool
+end
+
+mutable struct DeferredNode
+    op::Symbol
     possible_vars::Set{Label}
     possible_overlap::Set{Label}
     maybe_const_true::Bool
@@ -20,55 +24,23 @@ mutable struct BDD
     strict_bdd::Union{InnerBDD, Nothing}
 end
 
-# function bdd_is_false(bdd::BDD)
-#     # if it could be false, we need to force it
-#     if bdd_maybe_const_false(bdd)
-#         return bdd_is_false(force(bdd))
-#     end
-#     return false
-# end
 
-# function bdd_maybe_const_false(bdd::BDD)
-#     head = bdd.head
+function maybe_const_true(bdd::BDD)
+    node = bdd.node :: DeferredNode
+    bdd.negated ? node.maybe_const_false : node.maybe_const_true
+end
 
-#     # if forced, we just look at the inner bdd
-#     head isa Forced && return bdd_is_false(head.bdd)
-
-#     if head isa DeferredNot
-#         return bdd_maybe_const_true(bdd.args[1])
-#     end
-
-#     if head isa DeferredAnd
-#         a = bdd.args[1]
-#         b = bdd.args[2]
-#         # the bdd is false if either child is false
-#         bdd_maybe_const_false(a) && return true
-#         bdd_maybe_const_false(b) && return true
-#         # if neither is false and they have no overlap, then the bdd cant be false
-#         isempty(bdd.possible_overlap) && return false
-#         # if there is overlap, we need to force ourselves
-#         return bdd_is_false(force(bdd))
-#     end
-
-#     if head isa DeferredOr
-#         a = bdd.args[1]
-#         b = bdd.args[2]
-#         # the bdd is false if either child is false
-#         bdd_is_false(a) && return true
-#         bdd_is_false(b) && return true
-#         # if neither is false and they have no overlap, then the bdd cant be false
-#         isempty(bdd.possible_overlap) && return false
-#         # if there is overlap, we need to force ourselves
-#         return bdd_is_false(force(bdd))
-#     end
-# end
-
+function maybe_const_false(bdd::BDD)
+    node = bdd.node :: DeferredNode
+    bdd.negated ? node.maybe_const_true : node.maybe_const_false
+end
 
 function bdd_is_false(a::BDD)
-    if !isnothing(a.strict_bdd)
-        return bdd_is_false(a.strict_bdd)
-    end
-    if !a.maybe_const_false
+    # strict_false = bdd_is_false(a.strict_bdd)
+    # if !isnothing(a.strict_bdd)
+    #     return bdd_is_false(a.strict_bdd)
+    # end
+    if !maybe_const_false(a)
         return false
     end
     return bdd_is_false(force(a))
@@ -76,10 +48,10 @@ end
 
 
 function bdd_is_true(a::BDD)
-    if !isnothing(a.strict_bdd)
-        return bdd_is_true(a.strict_bdd)
-    end
-    if !a.maybe_const_true
+    # if !isnothing(a.strict_bdd)
+    #     return bdd_is_true(a.strict_bdd)
+    # end
+    if !maybe_const_true(a)
         return false
     end
     return bdd_is_true(force(a))
@@ -89,50 +61,41 @@ function force(dbdd::BDD)::InnerBDD
     if dbdd.strict_bdd !== nothing
         return dbdd.strict_bdd
     end
-    if dbdd.head isa DeferredAnd
-        a = force(dbdd.args[1])
-        if bdd_is_false(a) # perhaps unnecessary optimization but yeah
-            set_forced(dbdd, a)
-        else
-            b = force(dbdd.args[2])
-            set_forced(dbdd, a & b)
-        end
-    elseif dbdd.head isa DeferredOr
-        a = force(dbdd.args[1])
-        if bdd_is_true(a)
-            set_forced(dbdd, a)
-        else
-            b = force(dbdd.args[2])
-            set_forced(dbdd, a | b)
-        end
-    elseif dbdd.head isa DeferredNot
-        a = force(dbdd.args[1])
-        set_forced(dbdd, !a)
+    a = force(dbdd.left)
+    if bdd_is_false(a) # perhaps unnecessary optimization but yeah
+        set_strict(dbdd, a)
+    else
+        b = force(dbdd.right)
+        set_strict(dbdd, a & b)
     end
     @assert dbdd.head isa Forced
     dbdd.head.bdd
 end
 
-function set_forced(dbdd::BDD, bdd::InnerBDD)
-    dbdd.head = Forced(bdd)
-    dbdd.possible_vars = bdd_get_vars(bdd)
-    dbdd.maybe_const_true = bdd_is_true(bdd)
-    dbdd.maybe_const_false = bdd_is_false(bdd)
-    empty!(dbdd.args)
+function set_strict(node::DeferredNode, bdd::InnerBDD)
+    node.strict_bdd = bdd
+    node.possible_vars = bdd_get_vars(bdd)
+    node.maybe_const_true = bdd_is_true(bdd)
+    node.maybe_const_false = bdd_is_false(bdd)
+    node.left = nothing
+    node.right = nothing
 end
 
 function embed_bdd(bdd::InnerBDD)::BDD
-    return BDD(Forced(bdd), bdd_get_vars(bdd), Set{Label}(),bdd_is_true(bdd), bdd_is_false(bdd), BDD[], bdd)
+    node = DeferredNode(:embedded, bdd_get_vars(bdd), Set{Label}(),bdd_is_true(bdd), bdd_is_false(bdd), nothing, nothing, bdd)
+    return BDD(node, false)
 end
 
 
 function bdd_and(a::BDD, b::BDD)
-    possible_vars = a.possible_vars ∪ b.possible_vars
-    possible_overlap = a.possible_overlap ∩ b.possible_overlap
-    maybe_const_true = a.maybe_const_true && b.maybe_const_true
-    maybe_const_false = a.maybe_const_false || b.maybe_const_false || !isempty(possible_overlap)
-    strict_bdd = bdd_and(a.strict_bdd, b.strict_bdd)
-    return BDD(DeferredAnd(), possible_vars, possible_overlap, maybe_const_true, maybe_const_false, [a, b], strict_bdd)
+    a_node = a.node :: DeferredNode
+    b_node = b.node :: DeferredNode
+    possible_vars = a_node.possible_vars ∪ b_node.possible_vars
+    possible_overlap = a_node.possible_vars ∩ b_node.possible_vars
+    maybe_const_true = maybe_const_true(a) && maybe_const_true(b)
+    maybe_const_false = maybe_const_false(a) || maybe_const_false(b) || !isempty(possible_overlap)
+    node = DeferredNode(:and, possible_vars, possible_overlap, maybe_const_true, maybe_const_false, a, b, nothing)
+    return BDD(node, false)
 end
 
 # note you could also write Or just as !(!a & !b) which if you had complement pointers wouldnt be bad
@@ -149,7 +112,7 @@ end
 function bdd_negate(a::BDD)
     strict_bdd = bdd_negate(a.strict_bdd)
     # maybe true and maybe false swap
-    return BDD(DeferredNot(), a.possible_vars, Set{Label}(), a.maybe_const_false, a.maybe_const_true, [a], strict_bdd)
+    return BDD(a.node, !a.negated)
 end
 
 bdd_implies(a::BDD, b::BDD) = b | !a
