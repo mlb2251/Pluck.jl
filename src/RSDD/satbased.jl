a0318d768a2219cde0cd63ed3a52c8ab4be0a1ff
Maddy Bowers <mlbowers@mit.edu>
2026-03-26 13:06:05 -0400

    satbased

--- src/RSDD/satbased.jl
+++ src/RSDD/satbased.jl
@@ -1,146 +1,101 @@
 export var_bdd, true_bdd, false_bdd
 const DEBUG_CHECKS = false
 
-mutable struct BDD
-    node
-    negated::Bool
+mutable struct DeferredBDD
+    op::Symbol
+    left::Union{DeferredBDD, Nothing}
+    right::Union{DeferredBDD, Nothing}
+    strict_bdd::Union{InnerBDD, Nothing}
 end
 
-node(bdd::BDD) :: DeferredNode = bdd.node
-
-mutable struct DeferredNode
-    op::Symbol
-    min_var::Int
-    max_var::Int
-    maybe_const_true::Bool
-    maybe_const_false::Bool
-    left::Union{BDD, Nothing}
-    right::Union{BDD, Nothing}
-    strict_bdd::Union{InnerBDD, Nothing}
+mutable struct BDD
+    bdd::DeferredBDD
     sat_expr::SATExpr
 end
 
-function is_const_true(bdd::BDD)
-    node = bdd.node :: DeferredNode
-    return bdd.negated ? node.op === :F : node.op === :T
-end
-
-function is_const_false(bdd::BDD)
-    node = bdd.node :: DeferredNode
-    return bdd.negated ? node.op === :T : node.op === :F
-end
-
-
-function maybe_const_true(bdd::BDD)
-    node = bdd.node :: DeferredNode
-    bdd.negated ? node.maybe_const_false : node.maybe_const_true
-end
-
-function maybe_const_false(bdd::BDD)
-    node = bdd.node :: DeferredNode
-    bdd.negated ? node.maybe_const_true : node.maybe_const_false
-end
 
 function bdd_is_false(a::BDD)
     if DEBUG_CHECKS
-        if bdd_is_false(force(a))
-            @assert maybe_const_false(a)
+        if bdd_is_false(force(a.bdd))
+            # @assert maybe_const_false(a)
         end
     end
-    if is_const_false(a)
-        return true
-    end
-    if !maybe_const_false(a)
-        return false
-    end
-
-    # if sat_check(get_sat_expr(a)) === :sat
-    #     return false
-    # end
-
-    return bdd_is_false(force(a))
+    a.sat_expr.head === :F && return true
+    (a.sat_expr.head === :T || a.sat_expr.head === :var) && return false
+    return sat_check(a.sat_expr) === :unsat
 end
 
-bdd_is_true(a::BDD) = bdd_is_false(!a)
-
-function force(bdd::BDD)::InnerBDD
-    node = bdd.node :: DeferredNode
-    if node.strict_bdd !== nothing
-        return bdd.negated ? bdd_negate(node.strict_bdd) : node.strict_bdd
-    end
-    a = force(node.left)
-    if bdd_is_false(a) # perhaps unnecessary optimization but yeah
-        set_strict(node, a) # F & _ = F
-    elseif bdd_is_true(a)
-        set_strict(node, force(node.right))
-    else
-        b = force(node.right)
-        set_strict(node, a & b)
-    end
-    return bdd.negated ? bdd_negate(node.strict_bdd) : node.strict_bdd
+function bdd_is_true(a::BDD)
+    return bdd_is_false(bdd_negate(a))
 end
 
-function set_strict(node::DeferredNode, bdd::InnerBDD)
-    node.strict_bdd = bdd
-    is_true = bdd_is_true(bdd)
-    is_false = bdd_is_false(bdd)
-    node.op = is_true ? :T : is_false ? :F : node.op
-    node.min_var = is_true || is_false ? typemax(Int) : bdd_topvar(bdd)
-    node.max_var = is_true || is_false ? typemin(Int) : node.max_var # we dont know the max var
-    node.maybe_const_true = is_true
-    node.maybe_const_false = is_false
-    node.left = nothing
-    node.right = nothing
+
+function force(deferred::DeferredBDD)::InnerBDD
+    if deferred.strict_bdd !== nothing
+        return deferred.strict_bdd
+    end
+    a = force(deferred.left)
+
+    if deferred.op === :and
+        if bdd_is_false(a)
+            deferred.strict_bdd = a # F & _ = F
+        elseif bdd_is_true(a)
+            deferred.strict_bdd = force(deferred.right)
+        else
+            b = force(deferred.right)
+            deferred.strict_bdd = bdd_and(a, b)
+        end
+    elseif deferred.op === :or
+        if bdd_is_true(a)
+            deferred.strict_bdd = a # T | _ = T
+        elseif bdd_is_false(a)
+            deferred.strict_bdd = force(deferred.right)
+        else
+            b = force(deferred.right)
+            deferred.strict_bdd = bdd_or(a, b)
+        end
+    elseif deferred.op === :not
+        deferred.strict_bdd = bdd_negate(force(deferred.left))
+    end
+    return deferred.strict_bdd
 end
 
 function var_bdd(bdd::InnerBDD, label::Int)::BDD
-    node = DeferredNode(:var, label, label, false, false, nothing, nothing, bdd, sat_var(label))
-    return BDD(node, false)
+    deferred = DeferredBDD(:var, nothing, nothing, bdd)
+    return BDD(deferred, sat_var(label))
 end
 
 function true_bdd(bdd::InnerBDD)::BDD
-    node = DeferredNode(:T, typemax(Int), typemin(Int), true, false, nothing, nothing, bdd, SAT_TRUE)
-    return BDD(node, false)
+    deferred = DeferredBDD(:T, nothing, nothing, bdd)
+    return BDD(deferred, SAT_TRUE)
 end
 
 function false_bdd(bdd::InnerBDD)::BDD
-    node = DeferredNode(:F, typemax(Int), typemin(Int), false, true, nothing, nothing, bdd, SAT_FALSE)
-    return BDD(node, false)
-end
-
-function get_sat_expr(a::BDD)::SATExpr
-    node = a.node :: DeferredNode
-    return a.negated ? sat_not(node.sat_expr) : node.sat_expr
+    deferred = DeferredBDD(:F, nothing, nothing, bdd)
+    return BDD(deferred, SAT_FALSE)
 end
 
 function bdd_and(a::BDD, b::BDD)
-    is_const_true(a) && return b
-    is_const_true(b) && return a
-    is_const_false(a) && return a
-    is_const_false(b) && return b
-
-    a_node = a.node :: DeferredNode
-    b_node = b.node :: DeferredNode
-    interval_overlap = a_node.min_var != typemax(Int) && b_node.min_var != typemax(Int) && (a_node.min_var <= b_node.max_var && b_node.min_var <= a_node.max_var)
-
-    min_var = min(a_node.min_var, b_node.min_var)
-    max_var = max(a_node.max_var, b_node.max_var)
-    maybe_const_true_val = maybe_const_true(a) && maybe_const_true(b)
-    maybe_const_false_val = maybe_const_false(a) || maybe_const_false(b) || interval_overlap
-
-    sat_expr = sat_and(get_sat_expr(a), get_sat_expr(b))
-    node = DeferredNode(:and, min_var, max_var, maybe_const_true_val, maybe_const_false_val, a, b, nothing, sat_expr)
-    return BDD(node, false)
+    deferred = DeferredBDD(:and, a.bdd, b.bdd, nothing)
+    return BDD(deferred, sat_and(a.sat_expr, b.sat_expr))
 end
 
-bdd_or(a::BDD, b::BDD) = !(!a & !b)
-bdd_negate(a::BDD) = BDD(a.node, !a.negated)
+function bdd_or(a::BDD, b::BDD)
+    deferred = DeferredBDD(:or, a.bdd, b.bdd, nothing)
+    return BDD(deferred, sat_or(a.sat_expr, b.sat_expr))
+end
+
+function bdd_negate(a::BDD)
+    deferred = DeferredBDD(:not, a.bdd, nothing, nothing)
+    return BDD(deferred, sat_not(a.sat_expr))
+end
+
 bdd_implies(a::BDD, b::BDD) = b | !a
 Base.:!(a::BDD) = bdd_negate(a)
 Base.:&(a::BDD, b::BDD) = bdd_and(a, b)
 Base.:|(a::BDD, b::BDD) = bdd_or(a, b)
-bdd_topvar(a::BDD) = bdd_topvar(force(a))
-bdd_size(a::BDD) = bdd_size(force(a))
-bdd_wmc(a::BDD) = bdd_wmc(force(a))
+bdd_topvar(a::BDD) = bdd_topvar(force(a.bdd))
+bdd_size(a::BDD) = bdd_size(force(a.bdd))
+bdd_wmc(a::BDD) = bdd_wmc(force(a.bdd))
 
 
